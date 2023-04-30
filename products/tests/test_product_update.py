b552165dcc2f79b1f1907ed88a942b0b665b8b50
EzequielK-source <programacionezequielk@gmail.com>
2023-04-30 17:55:18 -0300

    Add test for product_update user case - Added a testcase for verify the render of product_update template

---
+++ products/tests/test_product_update.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from products.models import Products
+
+
+class ProductUpdateTestCase(TestCase):
+    '''
+      Verify the correct functionality of ProductUpdate user cases
+      following the next testcases:
+        * product_update path render product_update template
+        * if product not exist render product_update template but
+        with error message
+        * verify if product was updated in the db
+    '''
+
+    def setUp(self) -> None:
+        self.productSuject = Products(None, 'test_product')
+        self.productSuject.save()
+        self.data = {
+            'name': 'a new name'
+        }
+
+    def test_product_update_route_render_product_update_template(self):
+        url = reverse('product_update', args=[self.productSuject.id])
+        response = self.client.get(url, data=self.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'product_update.html')
