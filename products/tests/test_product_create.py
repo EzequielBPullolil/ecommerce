a0dfd47b9cc27ba2c28fa92039b93463fe844c10
EzequielK-source <programacionezequielk@gmail.com>
2023-04-30 16:18:19 -0300

    Refact ProductCreate Test - Add testcase for get request - Add testcase for post request

--- products/tests/test_product_create.py
+++ products/tests/test_product_create.py
@@ -1,15 +1,44 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from products.models import Products
+from products.forms import ProductForm
+
 
 class ProductCreateTestCase(TestCase):
     def setUp(self):
         self.url = reverse('product_create')
+        self.data = {
+            'name': 'Test product',
+            'description': 'A test product',
+            'price': 10.99,
+            'quantity': 20,
+        }
 
-    def test_product_create_with_get_method_return_view(self):
+    def test_get(self):
         '''
-            Verify if request product_create route return product_create view
+            Request products/create route and check if they return the
+            product_create template
         '''
         response = self.client.get(self.url)
-
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'product_create.html')
+        self.assertIsInstance(response.context['form'], ProductForm)
+
+    def test_post(self):
+        '''
+            Request products/create route with POST method and verify 
+            the next testcases:
+                * Response Status_code 302 
+                * Product persist
+                * Render product_detail
+        '''
+
+        response = self.client.post(self.url, data=self.data)
+        self.assertEqual(response.status_code, 302)
+        product = Products.objects.first()
+
+        self.assertIsNotNone(product)
+
+        self.assertRedirects(response, reverse(
+            'product_detail', args=[product.id]))
