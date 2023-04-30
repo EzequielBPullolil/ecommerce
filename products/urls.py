55c6fd3492d1eae7edfafaf4bdeebeaa1ae7eedc
EzequielK-source <programacionezequielk@gmail.com>
2023-04-30 18:45:43 -0300

    Add product_delete path and linked with ProductDeleteView

--- products/urls.py
+++ products/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from .views import (
     ProductDetailView,
     ProductCreateView,
-    ProductUpdateView
+    ProductUpdateView,
+    ProductDeleteView
 )
 urlpatterns = [
     path('create/', ProductCreateView.as_view(), name='product_create'),
     path('<int:id>/', ProductDetailView.as_view(), name='product_detail'),
-    path('<int:id>/update', ProductUpdateView.as_view(), name='product_update')
+    path('<int:id>/update', ProductUpdateView.as_view(), name='product_update'),
+    path('<int:id>/delete', ProductDeleteView.as_view(), name='product_delete'),
+
 ]
