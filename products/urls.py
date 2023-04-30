d6309653f2d3d711a56fd104dde05757161fb365
EzequielK-source <programacionezequielk@gmail.com>
2023-04-30 17:18:33 -0300

    Add product_update path linked with ProductUpdateView

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
     ProductDetailView,
-    ProductCreateView
+    ProductCreateView,
+    ProductUpdateView
 )
 urlpatterns = [
     path('create/', ProductCreateView.as_view(), name='product_create'),
-    path('<int:id>/', ProductDetailView.as_view(), name='product_detail')
+    path('<int:id>/', ProductDetailView.as_view(), name='product_detail'),
+    path('<int:id>/update', ProductUpdateView.as_view(), name='product_update')
 ]
