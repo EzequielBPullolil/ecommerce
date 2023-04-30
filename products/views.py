f8de21d6dedb40ed4d89a3f3cb0d060775202f40
EzequielK-source <programacionezequielk@gmail.com>
2023-04-30 16:22:59 -0300

    Add ProductDetailView for product_detail - Replace functional create view for Generic Create view

--- products/views.py
+++ products/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
-
+from typing import Any, Dict, Optional
+from django.db import models
+from django.forms.models import BaseModelForm
+from django.shortcuts import redirect, render
+from products.models import Products
+from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+from .forms import ProductForm
+from django.urls import reverse_lazy
 # Create your views here.
 
 
-def create(request):
-    return render(request, 'product_create.html')
+class ProductCreateView(CreateView):
+    model = Products
+    form_class = ProductForm
+    template_name = 'product_create.html'
+
+    def get_object(self):
+        id = self.kwargs.get('id')
+        return Products.objects.get(id=id)
+
+    def get_success_url(self):
+        return reverse_lazy('product_detail', args=[self.object.id])
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        response = super().form_valid(form)
+        self.object = form.save()
+        return redirect(reverse_lazy('product_detail', args=[self.object.id]))
+
+
+class ProductDetailView(DetailView):
+    model = Products
+    template_name = 'product_detail.html'
+    context_object_name = 'product'
+    pk_url_kwarg = 'id'
