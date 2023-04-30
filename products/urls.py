cf3216e59140a78e89bebdade36cabb822f35f13
EzequielK-source <programacionezequielk@gmail.com>
2023-04-30 20:51:39 -0300

    Added path to product_home and link with ProductHomeView

--- products/urls.py
+++ products/urls.py
@@ -3,12 +3,14 @@ from .views import (
     ProductDetailView,
     ProductCreateView,
     ProductUpdateView,
-    ProductDeleteView
+    ProductDeleteView,
+    ProductHomeView
 )
 urlpatterns = [
     path('create/', ProductCreateView.as_view(), name='product_create'),
     path('<int:id>/', ProductDetailView.as_view(), name='product_detail'),
     path('<int:id>/update', ProductUpdateView.as_view(), name='product_update'),
     path('<int:id>/delete', ProductDeleteView.as_view(), name='product_delete'),
+    path('/', ProductHomeView.as_view(), name='product_home'),
 
 ]
