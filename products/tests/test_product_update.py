92d8aa45bb5f938d36db5aebab300c1a2cb69ced
EzequielK-source <programacionezequielk@gmail.com>
2023-04-30 18:05:38 -0300

    Added testcase for product_update - Added a testcase for verify the render of product_not_found

--- products/tests/test_product_update.py
+++ products/tests/test_product_update.py
@@ -9,7 +9,7 @@ class ProductUpdateTestCase(TestCase):
       Verify the correct functionality of ProductUpdate user cases
       following the next testcases:
         * product_update path render product_update template
-        * if product not exist render product_update template but
+        * if product not exist render product_not_found template but
         with error message
         * verify if product was updated in the db
     '''
@@ -26,3 +26,9 @@ class ProductUpdateTestCase(TestCase):
         response = self.client.get(url, data=self.data)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'product_update.html')
+
+    def test_not_found_product_render_template_product_not_found(self):
+        url = reverse('product_update', args=[2002020320])
+        response = self.client.get(url, data=self.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'product_not_found.html')
