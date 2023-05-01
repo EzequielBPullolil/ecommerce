ab2586dc22e44a2a87ef2e29af3369b36bfaee6b
EzequielK-source <programacionezequielk@gmail.com>
2023-05-01 17:48:21 -0300

    Add email column to Users model

--- users/models.py
+++ users/models.py
@@ -7,3 +7,5 @@ class Users(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     password = models.CharField(max_length=64)
+    email = models.EmailField(
+        unique=True, default='bad_user_register@error.com')
