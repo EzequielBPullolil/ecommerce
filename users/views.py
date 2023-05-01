8a776b95047e3a6a9b8e3231f0263ab579b5cea0
EzequielK-source <programacionezequielk@gmail.com>
2023-05-01 18:13:36 -0300

    Add render in UserRegisterView post method - Create user_error.html - now UserRegisterView post method render user_error.html

--- users/views.py
+++ users/views.py
@@ -1,8 +1,20 @@
+from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from users.forms import UserForm
+from users.models import Users
 # Create your views here.
 
 
 class UserRegisterView(CreateView):
+    model = Users
     form_class = UserForm
     template_name = 'user_register.html'
+
+    def post(self, request, *args, **kwargs):
+        '''
+            Validate obtained form fields and 
+            persist user
+            * If they email are already registered
+              render user_error.html template
+        '''
+        return render(request, 'user_error.html')
